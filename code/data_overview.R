1ded49148dc298c826074f8aed8b9036a8c58b7b
chapinux <paul.chapron@gmail.com>
2024-10-08 17:54:34 +0200

    data en geojson + code d'apperçu

---
+++ code/data_overview.R
@@ -0,0 +1,142 @@
+
+# chargement des packages utiles 
+library(sf) # manip données spatiales
+library(ggplot2) # graphiques
+library(dplyr) #manipulation de dataframe 
+library(geojsonR) # entrée/sortie avec format geojson
+library(leaflet) #carto web 
+library(raster) #raster
+library(rgdal) #calcul raster
+library(stars) #manip données spatiotemporelles  
+library(MASS)
+
+
+
+# définition répertoire de travail 
+setwd("~/encadrement/PIR_2024_2025/KDE_pathfinding/") 
+
+
+
+#chargement des données 
+boutiques <- st_read("./data/BOUTIQUES.shp")
+restaurants <- st_read("./data/RESTAURANTS.shp")
+tramway <- st_read("./data/TRAMWAY.shp")
+zonepieton <- st_read("./data/ZPB_EXPLODE2.shp")
+buildings <-  st_read("./data/BUILDINGS_NS.geojson")
+
+
+#ajout d'un type pour les légendes
+
+zonepieton$type <-  "zone marchable"
+boutiques$type <-  "boutiques"
+tramway$type <-  "arrêt de tram"
+restaurants$type <- "restaurants"
+buildings$type <- "bâti"
+
+
+# projection Lambert 93 
+st_crs(boutiques) <-  2154
+st_crs(tramway) <-  2154
+st_crs(zonepieton) <-  2154
+st_crs(restaurants) <-  2154
+st_crs(buildings) <-  2154
+
+
+# affichage simple des géométries (sf)
+plot(zonepieton$geometry, border="grey30")
+plot(boutiques$geometry, add=T, col="orange", pch=18, cex=0.9)
+plot(tramway$geometry, add=T, col="blue", pch=18, cex=0.9)
+plot(restaurants$geometry, add=T, col="darkcyan", pch=18, cex=0.9)
+
+
+
+
+
+# on met tous les points d'interêt dans un seul dataframe
+
+# on vérifie que les PK (ID) sont bien uniques 
+all_IDs <-  c(tramway$PK, restaurants$PK, boutiques$PK)
+length(unique(all_IDs))== length(all_IDs)
+
+# on concatène les trois couches , il faut que le nombre de colonnes corresponde 
+# tramway a une colonne de plus restaurants et boutiques, on la retire 
+tramway <-  tramway %>%  dplyr::select(-OSM_ID)
+
+# concaténation des trois couches en une seule 
+POI <- rbind(restaurants,boutiques,tramway)
+
+
+
+# affichage plus complexe (ggplot2), avec légende automatique 
+# couleurs au hasard
+
+
+map1 <- ggplot(zonepieton)+
+  geom_sf(fill="slategray1", color="grey80", lwd=0.2)+
+  geom_sf(data=POI, aes(color= type, shape=type) )+
+  scale_color_manual(values=c("black", "orange", "#e2a2d2"))+
+  labs(color="Points d'intérêt \n (POI)",
+       shape="Points d'intérêt \n (POI)",
+       fill="Zone marchable")+
+  theme_void()
+map1
+
+
+
+#affichage avec leaflet (beaucoup moins maniable)
+
+# il faut reprojeter en WGS 84 
+POI84 <- st_transform(POI,4326)
+zonepieton84 <- st_transform(zonepieton,4326)
+
+POI$lat <-  st_coordinates(POI84) %>% pull(Y)
+POI$long <-  st_coordinates(POI84) %>% pull(X)
+
+map2 <-  leaflet(zonepieton84) %>% 
+  addTiles() %>%
+  addPolygons(weight=2) %>% 
+  addMarkers(data=POI84, )
+
+
+
+
+class(POI84)
+
+
+
+
+
+
+# fonction qui calcule le KDE de base (fonction kde2D du package MASS)
+
+st_kde <- function(points,cellsize, bandwith, extent = NULL){
+  require(MASS)
+  require(raster)
+  require(sf)
+  if(is.null(extent)){
+    extent_vec <- st_bbox(points)[c(1,3,2,4)]
+  } else{
+    extent_vec <- st_bbox(extent)[c(1,3,2,4)]
+  }
+  
+  n_y <- ceiling((extent_vec[4]-extent_vec[3])/cellsize)
+  n_x <- ceiling((extent_vec[2]-extent_vec[1])/cellsize)
+  
+  extent_vec[2] <- extent_vec[1]+(n_x*cellsize)-cellsize
+  extent_vec[4] <- extent_vec[3]+(n_y*cellsize)-cellsize
+  
+  coords <- st_coordinates(points)
+  matrix <- kde2d(coords[,1],coords[,2],h = bandwith,n = c(n_x,n_y),lims = extent_vec)
+  raster(matrix)
+}
+
+
+boutiques_dens <- st_kde(boutiques,20,300)
+restaurants_dens <- st_kde(restaurants,20,400)
+tramway_dens <- st_kde(tramway,20,400)
+
+plot(boutiques_dens)
+plot(restaurants_dens)
+plot(tramway_dens)
+
+
