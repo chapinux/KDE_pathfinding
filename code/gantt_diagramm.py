e342bbc47ff70f26fa04f4b8c716e1927bdaa65d
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2025-01-07 14:19:33 +0100

    Ajout gitlog et changement diagramme Gantt

--- code/gantt_diagramm.py
+++ code/gantt_diagramm.py
@@ -1,15 +1,24 @@
 import plotly.figure_factory as ff
 
 data = [
-    dict(Task="Initial Project Setup", Start="2023-06-13", Finish="2023-06-13"),
-    dict(Task="Project Presentation Preparation", Start="2023-10-18", Finish="2023-10-18"),
-    dict(Task="Data Acquisition and GeoJSON Processing", Start="2024-10-08", Finish="2024-10-09"),
-    dict(Task="A* Algorithm Implementation", Start="2024-10-22", Finish="2024-10-23"),
-    dict(Task="Pathfinding with Dijkstra's Algorithm", Start="2024-10-23", Finish="2024-11-11"),
-    dict(Task="Grid and Heuristics Optimization", Start="2024-11-12", Finish="2024-11-26"),
-    dict(Task="Raster to Graph Conversion", Start="2024-11-13", Finish="2024-12-04"),
-    dict(Task="Performance Improvements and JPS implementation", Start="2024-12-05", Finish="2024-12-06"),
+    dict(Task="Explication du sujet, compréhension du travail demandé et du problème associé", Start="2024-10-02", Finish="2024-10-14"),
+    dict(Task="Recherche bibliographique sur le sujet : Pathfinding 2D", Start="2024-10-02", Finish="2024-10-16"),
+    dict(Task="Identification et sélection des algorithmes de pathfinding 2D les plus adaptés", Start="2024-10-02", Finish="2024-10-17"),
+    dict(Task="Recherche bibliographique approfondie sur les algorithmes A* et Dijkstra", Start="2024-10-23", Finish="2024-11-14"),
+    dict(Task="Implémentation de base des algorithmes A* et Dijkstra", Start="2024-10-23", Finish="2024-11-15"),
+    dict(Task="Recherche bibliographique sur l'algorithme JPS", Start="2024-11-06", Finish="2024-11-15"),
+    dict(Task="Implémentation de JPS", Start="2024-11-06", Finish="2024-12-15"),
+    dict(Task="Développement du code pour convertir le raster en graphe", Start="2024-11-15", Finish="2024-12-18"),
+    dict(Task="Tests initiaux des algorithmes Dijkstra et A*", Start="2024-12-11", Finish="2024-12-30"),
+    dict(Task="Amélioration du raster de la zone praticable", Start="2024-12-19", Finish="2024-12-19"),
+    dict(Task="Benchmarking des algorithmes", Start="2024-12-19", Finish="2024-12-19"),
+    dict(Task="Changement dans le graphe utilisé par Dijkstra", Start="2025-01-05", Finish="2025-01-07"),
+    dict(Task="Rédaction des rapports et préparation des diapositives de présentation", Start="2025-01-05", Finish="2025-01-07"),
 ]
+
+
+
+
 
 
 
